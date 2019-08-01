87f38a008ca85db726646f22474095225d1f36f7
AliAbbasi <abbasi.ali.tab@gmail.com>
2019-08-01 23:41:00 +0300

    * tensorboard summary is added * weight saving and loading capability is added

--- train.py
+++ train.py
@@ -7,21 +7,24 @@ import numpy as np
 
 #----------------------------------------------------------------------------------------------------------------------
 
-# Basic model parameters as external flags.
+## basic model parameters as external flags.
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float  ('learning_rate', 0.0001,                                       "Initial learning rate.")
 flags.DEFINE_float  ('dropout',       0.5,                                          "Dropout probability.")
-flags.DEFINE_integer('max_steps',     1000000,                                        "Number of steps to run trainer.") 
+flags.DEFINE_integer('max_steps',     1000*1000,                                    "Number of steps to run trainer.") 
 flags.DEFINE_integer('batch_size',    32,                                           "Batch size. Must divide evenly into the dataset sizes.") 
 flags.DEFINE_string ('train_path',    "I:\\Cellular\\saved_npy_data\\train\\",      "train data path")
 flags.DEFINE_string ('valid_path',    "I:\\Cellular\\saved_npy_data\\validation\\", "validation data path")
-flags.DEFINE_integer('input_size0',   64,                                           "input data shape")
-flags.DEFINE_integer('input_size1',   64,                                           "input data shape")
+flags.DEFINE_integer('input_size0',   128,                                           "input data shape")
+flags.DEFINE_integer('input_size1',   128,                                           "input data shape")
 flags.DEFINE_integer('input_size2',   6,                                            "input data shape")
 flags.DEFINE_integer('output_size',   1108,                                         "Number of classes")
 flags.DEFINE_boolean('restore',       False,                                        "restore saved weights")
+flags.DEFINE_string ('weights',       "trained_weights_0",                          "restore saved weights")
 flags.DEFINE_boolean('train',         True,                                         "train of test phase")
+flags.DEFINE_string ('directory',     "saved_weights\\",                            "the directory for save weights" )
+flags.DEFINE_string ('logs',          "logs\\",                                     "the directory for save weights" )
 
 #----------------------------------------------------------------------------------------------------------------------
 
@@ -53,12 +56,15 @@ def main(_):
         with tf.Session(config=config) as sess:  
             sess.run(init_var) 
             
-            # restore model weights
+            summary_writer_train = tf.summary.FileWriter(FLAGS.logs + "train", graph=tf.get_default_graph())
+            summary_writer_valid = tf.summary.FileWriter(FLAGS.logs + "valid", graph=tf.get_default_graph()) 
+            
+            # restore model trained weights
             if FLAGS.restore:
-                if os.path.exists(directory + '/my-model.meta'): 
-                    new_saver = tf.train.import_meta_graph(directory + '/my-model.meta')
-                    new_saver.restore(sess, tf.train.latest_checkpoint(directory))  
-                    print ("\r\n------------ Saved weights restored. ------------")
+                if os.path.exists(FLAGS.directory + FLAGS.weights ): 
+                    new_saver = tf.train.import_meta_graph(FLAGS.directory + FLAGS.weights)
+                    new_saver.restore(sess, tf.train.latest_checkpoint(FLAGS.directory))  
+                    print ("\r\n------------ Trained weights restored. ------------")
             
             # prevent to add extra node to graph during training        
             tf.get_default_graph().finalize()  
@@ -66,14 +72,8 @@ def main(_):
             # get the updateable operation from graph for batch norm layer
             extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
             
-            # -------------- test phase --------------
-            if FLAGS.train == False:  
-                #get_results(sess)  # TODO 
-                print ("done")
-                sys.exit(0)
-                
             # -------------- train phase --------------
-            step         = 1  
+            step         = 1 
             train_cost   = []
             valid_cost   = []
             train_accu1  = []
@@ -82,49 +82,32 @@ def main(_):
             valid_accu2  = [] 
             accu1tr, accu2tr = 0, 0
             
+            ## main training loop
             while(step < FLAGS.max_steps):    
             
-                x_batch, y_batch = data_loader.get_batch_data(train_data, train_label, FLAGS.batch_size)  
-                # x_batch, y_batch = np.zeros((32,64,64,6), dtype=np.float32),  np.zeros((32, 1108), dtype=np.float32)
+                x_batch, y_batch = data_loader.get_batch_data(train_data, train_label, FLAGS.batch_size)   
                 
                 with tf.control_dependencies(extra_update_ops):  
-                    loss_i, _ = sess.run([deep_model.loss, deep_model.optimizer], feed_dict={x: x_batch, y: y_batch, keep_prob: FLAGS.dropout})    
-                    train_cost.append(loss_i) 
+                    sess.run([deep_model.optimizer], feed_dict={x: x_batch, y: y_batch, keep_prob: FLAGS.dropout})  
                 
                 # -------------- prints --------------
                 if step%500 == 0: 
-                    print ("%s , S:%3g , accu1: %4.3g , Cost: %2.3g "% ( str(datetime.datetime.now().time())[:-7], step, accu1tr, loss_i ))
+                    ## train loss and accuracy
+                    train_loss, train_accuracy, train_summary = sess.run([deep_model.loss, deep_model.accuracy, deep_model.sum], feed_dict={x: x_batch, y: y_batch, keep_prob: 1.0}) 
+                    
+                    ## validation loss and accuracy
+                    x_batch, y_batch = data_loader.get_batch_data(valid_data, valid_label, FLAGS.batch_size)  
+                    valid_loss, valid_accuracy, valid_summary = sess.run([deep_model.loss, deep_model.accuracy, deep_model.sum], feed_dict={x: x_batch, y: y_batch, keep_prob: 1.0}) 
+                    
+                    summary_writer_train.add_summary(train_summary, step) 
+                    summary_writer_valid.add_summary(valid_summary, step) 
+                    print ("%s, S:%5g, train_acc: %4.3g, train_loss: %2.3g, valid_acc: %4.3g, valid_loss: %2.3g"%
+                          (str(datetime.datetime.now().time())[:-7], step, train_accuracy*1., train_loss*1., valid_accuracy*1., valid_loss*1.))
                 
-                ## TODO: add accuracy
-                ## save the model
-                ## plot the cost and accuracy 
-                
-                ### -------------- accuracy calculator --------------  
-                ##if step % show_accuracy_step == 0 and show_accuracy:   
-                ##    accu1tr, accu2tr = accuFun(sess, x_batch, y_batch, batch_size)  
-                ##    train_accu1.append(accu1tr)
-                ##    train_accu2.append(accu2tr) 
-                ##    
-                ##    # valid accuray
-                ##    v_x_batch, v_y_batch = fetch_x_y(test_data, len(test_data)) 
-                ##    accu1v, accu2v = accuFun(sess, v_x_batch, v_y_batch, batch_size)  
-                ##    valid_accu1.append(accu1v)
-                ##    valid_accu2.append(accu2v)
-                ##    logging.info("accu1v: %4.3g , accu2v: %4.3g "% ( accu1v, accu2v ))
-                ##    print       ("accu1v: %4.3g , accu2v: %4.3g "% ( accu1v, accu2v ))
-                ##    
-                ### -------------- save mode, write cost and accuracy --------------  
-                ##if step % save_model_step == 0 and save_model: 
-                ##    logging.info("Saving the model...") 
-                ##    print       ("Saving the model...") 
-                ##    saver.save(sess, directory + '/my-model')
-                ##    logging.info("creating cost and accuray plot files...") 
-                ##    print       ("creating cost and accuray plot files...")
-                ##    utils.write_cost_accuray_plot(directory, train_cost, valid_cost, train_accu1, train_accu2, valid_accu1, valid_accu2) 
-                ##    
-                ### -------------- visualize scenes -------------- 
-                ##if step % visualize_scene_step == 0 and visualize_scene:
-                ##    show_result(sess)
+                # -------------- save weights --------------
+                if step%10000 == 0: 
+                    print ("saving the weights...!")
+                    saver.save(sess, FLAGS.directory + 'trained_weights_' + str(step))
                     
                 # --------------------------------------------- 
                 step += 1    
@@ -135,4 +118,5 @@ def main(_):
 
 if __name__ == '__main__': 
     tf.app.run()
-        
+    
+    
