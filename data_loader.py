6973d8c9e9b7078a67e24affb5dce396b55aa309
AliAbbasi <abbasi.ali.tab@gmail.com>
2019-08-04 21:21:41 +0300

    some augmenters are added

--- data_loader.py
+++ data_loader.py
@@ -17,23 +17,20 @@ def load_data_and_labels(train_path, valid_path, input_size):
     train_data  = np.zeros(shape=(train_data_count, input_size[0], input_size[0], 6), dtype=np.float16)
     train_label = np.zeros(shape=(train_data_count, 1),                               dtype=np.float16)
     valid_data  = np.zeros(shape=(valid_data_count, input_size[0], input_size[0], 6), dtype=np.float16)
-    valid_label = np.zeros(shape=(valid_data_count, 1),                               dtype=np.float16)
-    
-    max_sirna_value   = 1107.0
+    valid_label = np.zeros(shape=(valid_data_count, 1),                               dtype=np.float16) 
     
     ## LOAD DATA THERE ##
     train_data[:,:,:,:] = np.load(train_path +  "all_train_data_"+str(input_size[0])+".npy")
     train_label[:,:]    = np.load(train_path + "all_train_label_"+str(input_size[0])+".npy") 
+    print ("Train data is loaded.")
+    
     valid_data[:,:,:,:] = np.load(valid_path +  "all_valid_data_"+str(input_size[0])+".npy")
-    valid_label[:,:]    = np.load(valid_path + "all_valid_label_"+str(input_size[0])+".npy")
+    valid_label[:,:]    = np.load(valid_path + "all_valid_label_"+str(input_size[0])+".npy") 
+    print ("Validation data is loaded.")
     
     ## convert labels to one-hot vector 
     train_label = keras.utils.to_categorical(train_label, 1108)
     valid_label = keras.utils.to_categorical(valid_label, 1108)
-    
-    ## normalize the data
-    ## train_data = train_data/255.
-    ## valid_data = valid_data/255. 
     
     return train_data, train_label, valid_data, valid_label
     
@@ -53,17 +50,19 @@ def get_batch_data(data, label, batch_size):
     x, y = [], []  
     
     # s = time.time()
+    # counter = 0
     for i in prange(batch_size):   
         cur_x, cur_y = get_a_random_data(data, label)
         
-        ## augment the data with probability of 3/4
-        aug_type = random.randint(-1, 17)
+        ## augmentation
+        aug_type = random.randint(-1, 19) 
         if aug_type > -1:
             ## mixup
             if aug_type == 0: 
+                # counter += 1 
                 x2, y2 = get_a_random_data(data, label)
                 cur_x, cur_y = mix_up(cur_x, x2, cur_y, y2)
-            ## augmentation
+            ## other augmentations
             else:
                 cur_x = apply_augmentation(cur_x, aug_type)  
                 
@@ -75,6 +74,7 @@ def get_batch_data(data, label, batch_size):
     
     x = x/255. 
 
+    # print (counter)
     # print (time.time() - s)
     return x, y 
     
@@ -111,7 +111,7 @@ def apply_augmentation(x, aug_type):
     elif aug_type == 2:
         row, col, c= x.shape
         mean = 0
-        var = 0.1
+        var = 0.3
         sigma = var**0.5
         gauss = np.random.normal(mean,sigma,(row, col, c))
         gauss = gauss.reshape(row, col, c)
@@ -236,11 +236,36 @@ def apply_augmentation(x, aug_type):
         rand_x, rand_y = random.randint(0, x.shape[0]-rect_size-1), random.randint(0, x.shape[0]-rect_size-1)
         a_chan_image[rand_x:rand_x+rect_size, rand_y:rand_y+rect_size] = 0
         x[:,:,random_channel] = a_chan_image
+    
+    ## histogram equalization (1 channel)
+    elif aug_type == 18:
+        random_channel = random.randint(0, 5)
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        # hist, bins = np.histogram(a_chan_image.flatten(),256,[0,256]) 
+        # cdf = hist.cumsum()
+        # a_chan_image = cdf * hist.max()/ cdf.max()
+        a_chan_image = cv2.equalizeHist(a_chan_image)
+        x[:,:,random_channel] = a_chan_image
+    
+    ## brightness change (1 channel)
+    elif aug_type == 19:
+        random_channel = random.randint(0, 5)
+        a_chan_image =  x[:,:,random_channel]  #.astype(np.uint8)
+        
+        a_chan_image[np.where(a_chan_image < 255)] += float(random.randint(-15, 15))
+        x[:,:,random_channel] = a_chan_image
         
     else:
         pass
-        
-    ## TODO:  
+    
+    # for i in range(6):
+        # a_chan_image = (x[:,:,i]).astype(np.uint8)
+        # cv2.imwrite("aug_res\\_" +str(aug_type) + "_" + str(i) + ".png", a_chan_image ) 
+     
+    # print (aug_type)
+    # a =  input("enter here .")
+    
     ## 1 noise s&p
     ## 2 blur
     ## 3 4 5 rotations 90 180 270
@@ -251,14 +276,16 @@ def apply_augmentation(x, aug_type):
     ## 14 shear
     ## 15 poisson
     ## 16 speckle
-    ## 17 remove some part of data
+    ## 17 remove some part of data   
+    ## 18 hist equalization 
+    ## 19 brightness change
     
     ## mixed augmentaion is also possible
-   
     
+    ## TODO:  
     ## apply on 1, 2, 3, or all chennels 
     ## TODO: seperate the augmentation class and, write each augmentation as a function
-    ## TODO: visualize all  augmented images to check the result of them
+    ## TODO: visualize all augmented images to check the result of them
     
     return x 
     
