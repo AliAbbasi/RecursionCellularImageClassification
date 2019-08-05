cdf07aa2d2143bfda95d40f8d35e3d42655b7375
AliAbbasi <abbasi.ali.tab@gmail.com>
2019-08-05 23:37:19 +0300

    hyperparameters updated

--- train.py
+++ train.py
@@ -4,17 +4,19 @@ import datetime
 import tensorflow as tf
 from functools import reduce
 import numpy as np
-import os
+import os, shutil
 
 #----------------------------------------------------------------------------------------------------------------------
+
+## TODO: train on only one data
 
 ## basic model parameters as external flags.
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-flags.DEFINE_float  ('learning_rate', 0.00001,                                      "Initial learning rate.")
+flags.DEFINE_float  ('learning_rate', 0.01,                                      "Initial learning rate.")
 flags.DEFINE_float  ('dropout',       0.5,                                          "Dropout probability.")
 flags.DEFINE_integer('max_steps',     1000*10000,                                   "Number of steps to run trainer.") 
-flags.DEFINE_integer('batch_size',    64,                                           "Batch size. Must divide evenly into the dataset sizes.") 
+flags.DEFINE_integer('batch_size',    32,                                           "Batch size. Must divide evenly into the dataset sizes.") 
 flags.DEFINE_string ('train_path',    "I:\\Cellular\\saved_npy_data\\train\\",      "train data path")
 flags.DEFINE_string ('valid_path',    "I:\\Cellular\\saved_npy_data\\validation\\", "validation data path")
 flags.DEFINE_integer('input_size0',   128,                                          "input data shape")
@@ -29,7 +31,7 @@ flags.DEFINE_string ('logs',          "logs\\",                                 
 
 #---------------------------------------------------------------------------------------------------------------------- 
 
-def main(_): 
+def main(_):  
     train_data, train_label, valid_data, valid_label = data_loader.load_data_and_labels(FLAGS.train_path, FLAGS.valid_path, [FLAGS.input_size0, FLAGS.input_size1, FLAGS.input_size2])
     
     with tf.Graph().as_default():
@@ -70,7 +72,7 @@ def main(_):
             extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
             
             # -------------- train phase --------------
-            step         = 1 
+            step         = 0
             train_cost   = []
             valid_cost   = []
             train_accu1  = []
@@ -87,27 +89,28 @@ def main(_):
                 with tf.control_dependencies(extra_update_ops):  
                     sess.run([deep_model.optimizer], feed_dict={x: x_batch, y: y_batch, keep_prob: FLAGS.dropout})  
                 
-                # -------------- prints --------------
-                if step%10 == 0: 
-                    ## train loss and accuracy
-                    train_loss, train_accuracy, train_summary = sess.run([deep_model.loss, deep_model.accuracy, deep_model.sum], feed_dict={x: x_batch, y: y_batch, keep_prob: 1.0}) 
-                    
-                    ## validation loss and accuracy
-                    x_batch, y_batch = data_loader.get_batch_data(valid_data, valid_label, FLAGS.batch_size)  
-                    valid_loss, valid_accuracy, valid_summary = sess.run([deep_model.loss, deep_model.accuracy, deep_model.sum], feed_dict={x: x_batch, y: y_batch, keep_prob: 1.0}) 
-                    
-                    summary_writer_train.add_summary(train_summary, step) 
-                    summary_writer_valid.add_summary(valid_summary, step) 
-                    print ("%s, S:%5g, train_acc: %4.3g, train_loss: %2.3g, valid_acc: %4.3g, valid_loss: %2.3g"%
-                          (str(datetime.datetime.now().time())[:-7], step, train_accuracy*1., train_loss*1., valid_accuracy*1., valid_loss*1.))
+                    # -------------- prints --------------
+                    if step%1 == 0: 
+                        ## train loss and accuracy
+                        train_loss, train_accuracy, train_summary = sess.run([deep_model.loss, deep_model.accuracy, deep_model.sum], feed_dict={x: x_batch, y: y_batch, keep_prob: 1.0}) 
+                        
+                        ## validation loss and accuracy
+                        x_batch, y_batch = data_loader.get_batch_data(valid_data, valid_label, FLAGS.batch_size)  
+                        valid_loss, valid_accuracy, valid_summary = sess.run([deep_model.loss, deep_model.accuracy, deep_model.sum], feed_dict={x: x_batch, y: y_batch, keep_prob: 1.0}) 
+                        
+                        summary_writer_train.add_summary(train_summary, step) 
+                        summary_writer_valid.add_summary(valid_summary, step) 
+                        print ("%s, S:%5g, train_acc: %4.3g, train_loss: %2.3g, valid_acc: %4.3g, valid_loss: %2.3g"%
+                              (str(datetime.datetime.now().time())[:-7], step, train_accuracy*1., train_loss*1., valid_accuracy*1., valid_loss*1.))
+                              
+                    # -------------- save weights --------------
+                    if step%10000 == 0: 
+                        print ("saving the weights...!")
+                        saver.save(sess, FLAGS.directory + 'trained_weights_' + str(step))
+                        
+                    # --------------------------------------------- 
+                    step += 1    
                 
-                # -------------- save weights --------------
-                if step%10000 == 0: 
-                    print ("saving the weights...!")
-                    saver.save(sess, FLAGS.directory + 'trained_weights_' + str(step))
-                    
-                # --------------------------------------------- 
-                step += 1    
                 
             print(" --- \r\n --- \r\n  Trainig process is done after " + str(FLAGS.max_steps) + " iterations. \r\n --- \r\n ---")
             
