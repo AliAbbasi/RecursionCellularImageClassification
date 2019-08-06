fix rotation and shearing

data_loader.py
@@ -38,40 +38,34 @@ def load_data_and_labels(train_path, valid_path, input_size):
 
 def get_a_random_data(data, label):
     rand_index = random.randint(0, data.shape[0]-1) 
-    x = data[rand_index].copy()
+    x = data[rand_index] .copy()
     y = label[rand_index].copy()
     
     return x, y
 
 #----------------------------------------------------------------------------------------------------------------------
 
-# @jit(parallel=True)
-def get_batch_data(data, label, batch_size):
+@jit(parallel=True)
+def get_batch_data(data, label, batch_size, do_aug):
     x, y = [], []  
     
-    # for i in prange(batch_size):   
-    for i in range(batch_size):   
-        print ("i: ", i)
-        # cur_x, cur_y = get_a_random_data(data, label)
+    for i in prange(batch_size):      
+        cur_x, cur_y = get_a_random_data(data, label)
         
-        cur_x =  data[1000].copy()
-        cur_y = label[1000 ].copy()
+        # cur_x = data[i] .copy()
+        # cur_y = label[i].copy()
         
-        
-        # augmentation
-        # aug_type = random.randint(-1, 19) 
-        aug_type = random.randint(1, 19) 
-        ## # aug_type = random.randint(-1, 0) 
-        ## # if aug_type > -1:
-        ##     ## mixup
-        ##     # if aug_type == 0:  
-        ##         # x2, y2 = get_a_random_data(data, label)
-        ##         # x2, y2 = data[i+1, :, :, :], label[i+1 ]
-        ##         # cur_x, cur_y = mix_up(cur_x, x2, cur_y, y2)
-        ##     ## other augmentations
-        ##     # else:
-        ##         # cur_x = apply_augmentation(cur_x, aug_type)  
-        cur_x = apply_augmentation(cur_x, aug_type)  
+        if do_aug:
+            # augmentation
+            aug_type = random.randint(-1, 19)   
+            if aug_type > -1:
+                ## mixup
+                if aug_type == 0:  
+                    x2,    y2    = get_a_random_data(data, label) 
+                    cur_x, cur_y = mix_up(cur_x, x2, cur_y, y2)
+                ## other augmentations
+                else:
+                    cur_x = apply_augmentation(cur_x, aug_type)  
         
         x.append(cur_x) 
         y.append(cur_y)  
@@ -96,14 +90,12 @@ def mix_up(x1, x2, y1, y2):
 #----------------------------------------------------------------------------------------------------------------------
 
 def apply_augmentation(x, aug_type): 
-    print ("aug_type", aug_type)
-    for i in range(6):
-        print(i)
-        a_chan_image = ((x[:,:,i]).copy()).astype(np.uint8)
-        cv2.imshow("a_chan_image", a_chan_image)
-        cv2.waitKey()
-        cv2.imwrite("aug_res\\_" +str(0) + "_" + str(i) + ".png", a_chan_image ) 
-    print ("--------------------")
+    
+    # for i in range(6):
+        # print(i)
+        # a_chan_image = ((x[:,:,i]).copy()).astype(np.uint8) 
+        # cv2.imwrite("aug_res\\_" +str(0) + "_" + str(i) + ".png", a_chan_image ) 
+    # print ("--------------------")
     
     ## noise s&p
     if aug_type == 1:  
@@ -126,7 +118,7 @@ def apply_augmentation(x, aug_type):
     elif aug_type == 2:
         row, col, c= x.shape
         mean = 0
-        var = 0.3
+        var = 25
         sigma = var**0.5
         gauss = np.random.normal(mean,sigma,(row, col, c))
         gauss = gauss.reshape(row, col, c)
@@ -166,45 +158,45 @@ def apply_augmentation(x, aug_type):
         a_chan_image = a_chan_image[random_x:random_x+x.shape[0], random_y:random_y+x.shape[0]]
         x[:,:,random_channel] = a_chan_image
         
-    ## translate right (on 1 channel)
+    ## translate down (on 1 channel)
     elif aug_type == 9:
         random_channel = random.randint(0, 5)
         a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
         
-        random_trans_value = random.randint(0, x.shape[0] // 6)
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
         
         canvas = np.zeros_like(a_chan_image)
         canvas[random_trans_value:, :] = a_chan_image[:x.shape[0]-random_trans_value, :]
         x[:,:,random_channel] = canvas
     
-    ## translate left (on 1 channel)
+    ## translate up (on 1 channel)
     elif aug_type == 10: 
         random_channel = random.randint(0, 5)
         a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
         
-        random_trans_value = random.randint(0, x.shape[0] // 6)
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
         
         canvas = np.zeros_like(a_chan_image)
         canvas[:x.shape[0]-random_trans_value, :] = a_chan_image[random_trans_value:, :]
         x[:,:,random_channel] = canvas
         
-    ## translate up (on 1 channel)
+    ## translate left (on 1 channel)
     elif aug_type == 11:
         random_channel = random.randint(0, 5)
         a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
         
-        random_trans_value = random.randint(0, x.shape[0] // 6)
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
         
         canvas = np.zeros_like(a_chan_image)
         canvas[:, :x.shape[0]-random_trans_value] = a_chan_image[:, random_trans_value:]
         x[:,:,random_channel] = canvas
     
-    ## translate down (on 1 channel)
+    ## translate right (on 1 channel)
     elif aug_type == 12:
         random_channel = random.randint(0, 5)
         a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
         
-        random_trans_value = random.randint(0, x.shape[0] // 6)
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
         
         canvas = np.zeros_like(a_chan_image)
         canvas[:, random_trans_value:] = a_chan_image[:, :x.shape[0]-random_trans_value]
@@ -213,9 +205,13 @@ def apply_augmentation(x, aug_type):
     ## rotate (on 1 channel)
     elif aug_type == 13:
         random_channel = random.randint(0, 5)
-        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        a_chan_image = (x[:,:,random_channel]) 
+        a_chan_image = a_chan_image.astype(np.uint8) 
+        rows, cols = a_chan_image.shape 
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),random.randint(-15, 15),1)
+        a_chan_image = cv2.warpAffine(a_chan_image,M,(cols,rows))
         
-        a_chan_image = transform.rotate(a_chan_image, random.randint(-10, 10))
+        # a_chan_image = a_chan_image.astype(np.float16)
         x[:,:,random_channel] = a_chan_image
         
     ## shearing (on 1 channel)
@@ -223,9 +219,13 @@ def apply_augmentation(x, aug_type):
         random_channel = random.randint(0, 5)
         a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
         
-        afine_tf = transform.AffineTransform(shear=0.2) 
-        a_chan_image = transform.warp(a_chan_image, inverse_map=afine_tf)
-        x[:,:,random_channel] = a_chan_image
+        shear_factor = (-0.2, 0.2)
+        shear_factor = random.uniform(*shear_factor)
+        M = np.array([[1, abs(shear_factor), 0],[0,1,0]]) 
+        nW =  a_chan_image.shape[1] + abs(shear_factor*a_chan_image.shape[0])  
+        a_chan_image = cv2.warpAffine(a_chan_image, M, (int(nW), a_chan_image.shape[0]))
+        
+        x[:,:,random_channel] = a_chan_image[:x.shape[0], :x.shape[0]]
     
     ## poisson
     elif aug_type == 15: 
@@ -255,11 +255,7 @@ def apply_augmentation(x, aug_type):
     ## histogram equalization (1 channel)
     elif aug_type == 18:
         random_channel = random.randint(0, 5)
-        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
-        
-        # hist, bins = np.histogram(a_chan_image.flatten(),256,[0,256]) 
-        # cdf = hist.cumsum()
-        # a_chan_image = cdf * hist.max()/ cdf.max()
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8) 
         a_chan_image = cv2.equalizeHist(a_chan_image)
         x[:,:,random_channel] = a_chan_image
     
@@ -274,12 +270,10 @@ def apply_augmentation(x, aug_type):
     else:
         pass
     
-    for i in range(6):
-        a_chan_image = (x[:,:,i]).astype(np.uint8)
-        cv2.imwrite("aug_res\\_" +str(aug_type) + "_" + str(i) + ".png", a_chan_image ) 
-     
-    # print (aug_type)
-    # a =  input("enter here .")
+    # for i in range(6):
+        # a_chan_image = (x[:,:,i]).astype(np.uint8)
+        # cv2.imwrite("aug_res\\_" +str(aug_type) + "_" + str(i) + ".png", a_chan_image ) 
+    
     
     ## 1 noise s&p
     ## 2 blur
@@ -293,15 +287,12 @@ def apply_augmentation(x, aug_type):
     ## 16 speckle
     ## 17 remove some part of data   
     ## 18 hist equalization 
-    ## 19 brightness change
-    
-    ## mixed augmentaion is also possible
+    ## 19 brightness change 
+    ## TODO:: mixed augmentaion is also possible
     
     ## TODO:  
     ## apply on 1, 2, 3, or all chennels 
-    ## TODO: seperate the augmentation class and, write each augmentation as a function
-    ## TODO: visualize all augmented images to check the result of them, remove the unnecessary ones
-    ##      question 1: why the data are different, while I get same data to show
+    ## TODO: seperate the augmentation class and, write each augmentation as a function 
     
     ## (top urgent) TODO >>>>>> train only on one batch of 256 data, and check if it converge or not
     ##              note down every configuration, steps, loss and accuarcy