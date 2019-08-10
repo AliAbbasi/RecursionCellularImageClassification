c243486d2a94e7aedaf4e93d334a8fd2571105a7
AliAbbasi <abbasi.ali.tab@gmail.com>
2019-08-10 22:18:18 +0300

    * modular augmentation * second time augmentaion is added

--- augmenter.py
+++ augmenter.py
@@ -30,7 +30,7 @@ def salt_and_pepper(x):
     num_pepper = np.ceil(amount* x.size * (1. - s_vs_p))
     coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in x.shape]
     out[tuple(coords)] = 1
-    x = out.copy()
+    x = out.copy() 
     
     return x
     
@@ -48,49 +48,71 @@ def blur(x):
 
 #----------------------------------------------------------------------------------------------------------------------
 
-def rotate(x, rot_count):
-    for i in range(rot_count):
-        x = np.rot90(x) 
+def rotate_90(x):
+    x = np.rot90(x) 
+    return x    
+
+#----------------------------------------------------------------------------------------------------------------------
+
+def rotate_180(x):
+    x = np.rot90(x) 
+    x = np.rot90(x) 
     return x    
     
 #----------------------------------------------------------------------------------------------------------------------
 
-def flip(x, direction):
-    if direction:
-        x = np.fliplr(x)
-    else:
-        x = np.flipud(x)
+def rotate_270(x):
+    x = np.rot90(x) 
+    x = np.rot90(x) 
+    x = np.rot90(x) 
+    return x    
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def flip_up(x): 
+    x = np.flipud(x)
     return x
 
 #----------------------------------------------------------------------------------------------------------------------
 
-def remove_some_part(x):
-    random_channel = random.randint(0, 5)
-    rect_size = x.shape[0] // 6
+def flip_right(x): 
+    x = np.fliplr(x)  
+    return x
     
-    a_chan_image = (x[:,:,random_channel]).astype(np.uint8) 
-    rand_x, rand_y = random.randint(0, x.shape[0]-rect_size-1), random.randint(0, x.shape[0]-rect_size-1)
-    a_chan_image[rand_x:rand_x+rect_size, rand_y:rand_y+rect_size] = 0
-    x[:,:,random_channel] = a_chan_image
+#----------------------------------------------------------------------------------------------------------------------
+
+def remove_some_part(x):
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s:
+        rect_size = x.shape[0] // 6 
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8) 
+        rand_x, rand_y = random.randint(0, x.shape[0]-rect_size-1), random.randint(0, x.shape[0]-rect_size-1)
+        a_chan_image[rand_x:rand_x+rect_size, rand_y:rand_y+rect_size] = 0
+        x[:,:,random_channel] = a_chan_image
     return x
     
 #----------------------------------------------------------------------------------------------------------------------
 
 def his_equ(x):
-    random_channel = random.randint(0, 5)
-    a_chan_image = (x[:,:,random_channel]).astype(np.uint8) 
-    a_chan_image = cv2.equalizeHist(a_chan_image)
-    x[:,:,random_channel] = a_chan_image
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8) 
+        a_chan_image = cv2.equalizeHist(a_chan_image)
+        x[:,:,random_channel] = a_chan_image
     return x
 
 #----------------------------------------------------------------------------------------------------------------------
 
 def brightness(x):
-    random_channel = random.randint(0, 5)
-    a_chan_image =  x[:,:,random_channel]  #.astype(np.uint8)
-    
-    a_chan_image[np.where(a_chan_image < 255)] += float(random.randint(-15, 15))
-    x[:,:,random_channel] = a_chan_image 
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image =  x[:,:,random_channel]  #.astype(np.uint8)
+        
+        a_chan_image[np.where(a_chan_image < 255)] += float(random.randint(-15, 15))
+        x[:,:,random_channel] = a_chan_image 
     return x
 
 #----------------------------------------------------------------------------------------------------------------------
@@ -105,135 +127,24 @@ def poisson(x):
 #----------------------------------------------------------------------------------------------------------------------
 
 def rescale_and_crop(x):
-    random_channel = random.randint(0, 5)
-    a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
-    
-    a_chan_image = cv2.resize(a_chan_image, (x.shape[0]*2, x.shape[0]*2))
-    random_x = random.randint(0, x.shape[0])
-    random_y = random.randint(0, x.shape[0])
-    a_chan_image = a_chan_image[random_x:random_x+x.shape[0], random_y:random_y+x.shape[0]]
-    x[:,:,random_channel] = a_chan_image
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        a_chan_image = cv2.resize(a_chan_image, (x.shape[0]*2, x.shape[0]*2))
+        random_x = random.randint(0, x.shape[0])
+        random_y = random.randint(0, x.shape[0])
+        a_chan_image = a_chan_image[random_x:random_x+x.shape[0], random_y:random_y+x.shape[0]]
+        x[:,:,random_channel] = a_chan_image
     return x
     
 #----------------------------------------------------------------------------------------------------------------------
 
-def apply_augmentation(x, aug_type): 
-    
-    # for i in range(6):
-        # print(i)
-        # a_chan_image = ((x[:,:,i]).copy()).astype(np.uint8) 
-        # cv2.imwrite("aug_res\\_" +str(0) + "_" + str(i) + ".png", a_chan_image ) 
-    # print ("--------------------")
-    
-    ## noise s&p
-    if aug_type == 1:   
-        x = salt_and_pepper(x)
-    
-    ## blur
-    elif aug_type == 2:
-        x = blur(x)
-    
-    ## 90 rotate
-    elif aug_type == 3:
-        x = rotate(x, 1)
-        
-    ## 180 rotate
-    elif aug_type == 4:
-        x = rotate(x, 2) 
-        
-    ## 270 rotate
-    elif aug_type == 5:
-        x = rotate(x, 3)
-        
-    ## horizontal flip
-    elif aug_type == 6:
-        x = flip(x, 0)
-    
-    ## vertical flip
-    elif aug_type == 7:
-        x = flip(x, 1)
-        
-    ## remove some part of data (one channel)
-    elif aug_type == 8:
-        x = remove_some_part(x)
-    
-    ## histogram equalization (1 channel)
-    elif aug_type == 9:
-        x = his_equ(x)
-    
-    ## brightness change (1 channel)
-    elif aug_type == 10:
-        x = brightness(x)
-    
-    ## poisson
-    elif aug_type == 11: 
-        x = poisson(x) 
-        
-    ## <><><><><><><><>><><><><><><><><><><><><><>><><><><><><><><><><><><><>><><><><><><><><><><><><><>><><><><><>
-        
-    ## rescale and crop (on 1 channel)
-    elif aug_type == 12:
-        x = rescale_and_crop(x)
-        
-    ## translate down (on 1 channel)
-    elif aug_type == 13:
-        random_channel = random.randint(0, 5)
-        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
-        
-        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
-        
-        canvas = np.zeros_like(a_chan_image)
-        canvas[random_trans_value:, :] = a_chan_image[:x.shape[0]-random_trans_value, :]
-        x[:,:,random_channel] = canvas
-    
-    ## translate up (on 1 channel)
-    elif aug_type == 14: 
-        random_channel = random.randint(0, 5)
-        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
-        
-        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
-        
-        canvas = np.zeros_like(a_chan_image)
-        canvas[:x.shape[0]-random_trans_value, :] = a_chan_image[random_trans_value:, :]
-        x[:,:,random_channel] = canvas
-        
-    ## translate left (on 1 channel)
-    elif aug_type == 15:
-        random_channel = random.randint(0, 5)
-        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
-        
-        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
-        
-        canvas = np.zeros_like(a_chan_image)
-        canvas[:, :x.shape[0]-random_trans_value] = a_chan_image[:, random_trans_value:]
-        x[:,:,random_channel] = canvas
-    
-    ## translate right (on 1 channel)
-    elif aug_type == 16:
-        random_channel = random.randint(0, 5)
-        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
-        
-        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
-        
-        canvas = np.zeros_like(a_chan_image)
-        canvas[:, random_trans_value:] = a_chan_image[:, :x.shape[0]-random_trans_value]
-        x[:,:,random_channel] = canvas
-    
-    ## rotate (on 1 channel)
-    elif aug_type == 17:
-        random_channel = random.randint(0, 5)
-        a_chan_image = (x[:,:,random_channel]) 
-        a_chan_image = a_chan_image.astype(np.uint8) 
-        rows, cols = a_chan_image.shape 
-        M = cv2.getRotationMatrix2D((cols/2,rows/2),random.randint(-15, 15),1)
-        a_chan_image = cv2.warpAffine(a_chan_image,M,(cols,rows))
-        
-        # a_chan_image = a_chan_image.astype(np.float16)
-        x[:,:,random_channel] = a_chan_image
-        
-    ## shearing (on 1 channel)
-    elif aug_type == 18:  
-        random_channel = random.randint(0, 5)
+def shearing(x):
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s:
         a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
         
         shear_factor = (-0.2, 0.2)
@@ -243,35 +154,136 @@ def apply_augmentation(x, aug_type):
         a_chan_image = cv2.warpAffine(a_chan_image, M, (int(nW), a_chan_image.shape[0]))
         
         x[:,:,random_channel] = a_chan_image[:x.shape[0], :x.shape[0]]
+    return x
     
+#----------------------------------------------------------------------------------------------------------------------
+
+def translate_down(x):
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
         
-    else:
-        pass
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
+        
+        canvas = np.zeros_like(a_chan_image)
+        canvas[random_trans_value:, :] = a_chan_image[:x.shape[0]-random_trans_value, :]
+        x[:,:,random_channel] = canvas
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def translate_up(x):
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
+        
+        canvas = np.zeros_like(a_chan_image)
+        canvas[:x.shape[0]-random_trans_value, :] = a_chan_image[random_trans_value:, :]
+        x[:,:,random_channel] = canvas
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def translate_left(x):
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
+        
+        canvas = np.zeros_like(a_chan_image)
+        canvas[:, :x.shape[0]-random_trans_value] = a_chan_image[:, random_trans_value:]
+        x[:,:,random_channel] = canvas
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def translate_right(x):
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
+        
+        canvas = np.zeros_like(a_chan_image)
+        canvas[:, random_trans_value:] = a_chan_image[:, :x.shape[0]-random_trans_value]
+        x[:,:,random_channel] = canvas
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def random_rotate(x): 
+    random_channel_count = random.randint(1,6)
+    random_channel_s = random.sample([0,1,2,3,4,5], k=random_channel_count)
+    for random_channel in random_channel_s: 
+        a_chan_image = (x[:,:,random_channel]) 
+        a_chan_image = a_chan_image.astype(np.uint8) 
+        rows, cols = a_chan_image.shape 
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),random.randint(-15, 15),1)
+        a_chan_image = cv2.warpAffine(a_chan_image,M,(cols,rows)) 
+        x[:,:,random_channel] = a_chan_image
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def apply_augmentation(x, aug_type): 
+    augmentation_functions = [salt_and_pepper, blur, rotate_90, rotate_180, rotate_270, flip_up, flip_right, 
+                              remove_some_part, his_equ, brightness, poisson, rescale_and_crop, translate_down, 
+                              translate_left, translate_right, translate_up, random_rotate, shearing]
+                              
+    random_augmentaion_function = random.choice(augmentation_functions)
+    
+    # print (augmentation_functions.index(random_augmentaion_function))
+    ## first augmentation
+    x = random_augmentaion_function(x)
     
     # for i in range(6):
         # a_chan_image = (x[:,:,i]).astype(np.uint8)
-        # cv2.imwrite("aug_res\\_" +str(aug_type) + "_" + str(i) + ".png", a_chan_image ) 
+        # cv2.imwrite("aug_res\\_" +str(0) + "_" + str(i) + ".png", a_chan_image ) 
+    
+    ## second augmentaion with probability 50
+    if random.randint(0,1):
+        augmentation_functions.remove(random_augmentaion_function)
+        random_augmentaion_function = random.choice(augmentation_functions) 
+        x = random_augmentaion_function(x)
+    
+    # for i in range(6):
+        # a_chan_image = (x[:,:,i]).astype(np.uint8)
+        # cv2.imwrite("aug_res\\_" +str(1) + "_" + str(i) + ".png", a_chan_image )  
+    # print ("--------------------------")
+    # a = input()
+    
+    return x
     
     
-    ## 1 noise s&p
-    ## 2 blur
-    ## 3 4 5 rotations 90 180 270
-    ## 6 7 flips horiz, verti.  
-    ## 8 scale  
-    ## 9, 10, 11, 12 translate     
-    ## 13 rotate 
-    ## 14 shear
-    ## 15 poisson 
-    ## 16 remove some part of data   
-    ## 17 hist equalization 
-    ## 18 brightness change 
+    ## 1 noise s&p 
+    ## 2 blur 
+    ## 3 90 rotate 
+    ## 4 180 rotate 
+    ## 5 270 rotate  
+    ## 6 horizontal flip 
+    ## 7 vertical flip 
+    ## 8 remove some part of data (on random channels)  
+    ## 9 histogram equalization (on random channels)  
+    ## 10 brightness change (on random channels)  
+    ## 11 poisson
+    ## 12 scale and crop 
+    ## 13 14 15 16 translate   
+    ## 17 rotate
+    ## 18 shearing  
     
-    ## TODO:: mixed augmentaion is also possible 
-    ## TODO:  
-    ## apply on 1, 2, 3, or all chennels   
-    ## TODO: train with aug type only between 1-11
-    
-    ## TODO: add @jit to all augmentaion functions
+    ## TODO: should we apply mixup to all data ??
     return x 
     
 #----------------------------------------------------------------------------------------------------------------------
