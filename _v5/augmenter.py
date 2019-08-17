9107cf9a1aec77c8baaa1e7f2d42eebd8907ca53
AliAbbasi <abbasi.ali.tab@gmail.com>
2019-08-17 13:40:25 +0300

    * training process monitoring esxel file with different models and hyperparameters are added. * new model _v5 is added with focal loss, no mixup, direct augmentaiton, higher capacity

---
+++ _v5/augmenter.py
@@ -0,0 +1,263 @@
+import numpy as np
+import random
+import time
+from numba import jit, prange 
+import cv2 
+
+#----------------------------------------------------------------------------------------------------------------------
+
+channel_count = 12
+channel_array = [0,1,2,3,4,5,6,7,8,9,10,11]
+
+#----------------------------------------------------------------------------------------------------------------------
+
+def mix_up(x1, x2, y1, y2): 
+    lam = np.random.beta(0.4, 0.4) 
+    mixed_x = lam * x1 + (1 - lam) * x2 
+    mixed_y = lam * y1 + (1 - lam) * y2  
+    return mixed_x, mixed_y
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def salt_and_pepper(x):
+    s_vs_p = 0.5
+    amount = 0.004
+    out = np.copy(x)   
+    
+    # Salt mode
+    num_salt = np.ceil(amount * x.size * s_vs_p)
+    coords = [np.random.randint(0, i - 1, int(num_salt)) for i in x.shape]
+    out[tuple(coords)] = 1
+    
+    # Pepper mode
+    num_pepper = np.ceil(amount* x.size * (1. - s_vs_p))
+    coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in x.shape]
+    out[tuple(coords)] = 1
+    x = out.copy() 
+    
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def blur(x):
+    row, col, c= x.shape
+    mean = 0
+    var = 25
+    sigma = var**0.5
+    gauss = np.random.normal(mean,sigma,(row, col, c))
+    gauss = gauss.reshape(row, col, c)
+    x = x + gauss   
+    return x
+
+#----------------------------------------------------------------------------------------------------------------------
+
+def rotate_90(x):
+    x = np.rot90(x) 
+    return x    
+
+#----------------------------------------------------------------------------------------------------------------------
+
+def rotate_180(x):
+    x = np.rot90(x) 
+    x = np.rot90(x) 
+    return x    
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def rotate_270(x):
+    x = np.rot90(x) 
+    x = np.rot90(x) 
+    x = np.rot90(x) 
+    return x    
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def flip_up(x): 
+    x = np.flipud(x)
+    return x
+
+#----------------------------------------------------------------------------------------------------------------------
+
+def flip_right(x): 
+    x = np.fliplr(x)  
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def remove_some_part(x):
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s:
+        rect_size = x.shape[0] // 6 
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8) 
+        rand_x, rand_y = random.randint(0, x.shape[0]-rect_size-1), random.randint(0, x.shape[0]-rect_size-1)
+        a_chan_image[rand_x:rand_x+rect_size, rand_y:rand_y+rect_size] = 0
+        x[:,:,random_channel] = a_chan_image
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def his_equ(x):
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8) 
+        a_chan_image = cv2.equalizeHist(a_chan_image)
+        x[:,:,random_channel] = a_chan_image
+    return x
+
+#----------------------------------------------------------------------------------------------------------------------
+
+def brightness(x):
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image =  x[:,:,random_channel]  #.astype(np.uint8)
+        
+        a_chan_image[np.where(a_chan_image < 255)] += float(random.randint(-15, 15))
+        x[:,:,random_channel] = a_chan_image 
+    return x
+
+#----------------------------------------------------------------------------------------------------------------------
+
+def poisson(x):
+    x = x.astype(np.uint8)
+    vals = len(np.unique(x))
+    vals = 2 ** np.ceil(np.log2(vals))
+    x = np.random.poisson(x * vals) / float(vals)
+    return x
+
+#----------------------------------------------------------------------------------------------------------------------
+
+def rescale_and_crop(x):
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        a_chan_image = cv2.resize(a_chan_image, (x.shape[0]*2, x.shape[0]*2))
+        random_x = random.randint(0, x.shape[0])
+        random_y = random.randint(0, x.shape[0])
+        a_chan_image = a_chan_image[random_x:random_x+x.shape[0], random_y:random_y+x.shape[0]]
+        x[:,:,random_channel] = a_chan_image
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def shearing(x):
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        shear_factor = (-0.2, 0.2)
+        shear_factor = random.uniform(*shear_factor)
+        M = np.array([[1, abs(shear_factor), 0],[0,1,0]]) 
+        nW =  a_chan_image.shape[1] + abs(shear_factor*a_chan_image.shape[0])  
+        a_chan_image = cv2.warpAffine(a_chan_image, M, (int(nW), a_chan_image.shape[0]))
+        
+        x[:,:,random_channel] = a_chan_image[:x.shape[0], :x.shape[0]]
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def translate_down(x):
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
+        
+        canvas = np.zeros_like(a_chan_image)
+        canvas[random_trans_value:, :] = a_chan_image[:x.shape[0]-random_trans_value, :]
+        x[:,:,random_channel] = canvas
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def translate_up(x):
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
+        
+        canvas = np.zeros_like(a_chan_image)
+        canvas[:x.shape[0]-random_trans_value, :] = a_chan_image[random_trans_value:, :]
+        x[:,:,random_channel] = canvas
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def translate_left(x):
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
+        
+        canvas = np.zeros_like(a_chan_image)
+        canvas[:, :x.shape[0]-random_trans_value] = a_chan_image[:, random_trans_value:]
+        x[:,:,random_channel] = canvas
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def translate_right(x):
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s:
+        a_chan_image = (x[:,:,random_channel]).astype(np.uint8)
+        
+        random_trans_value = random.randint(x.shape[0] // 10, x.shape[0] // 6)
+        
+        canvas = np.zeros_like(a_chan_image)
+        canvas[:, random_trans_value:] = a_chan_image[:, :x.shape[0]-random_trans_value]
+        x[:,:,random_channel] = canvas
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def random_rotate(x): 
+    random_channel_count = random.randint(1, channel_count)
+    random_channel_s = random.sample(channel_array, k=random_channel_count)
+    for random_channel in random_channel_s: 
+        a_chan_image = (x[:,:,random_channel]) 
+        a_chan_image = a_chan_image.astype(np.uint8) 
+        rows, cols = a_chan_image.shape 
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),random.randint(-15, 15),1)
+        a_chan_image = cv2.warpAffine(a_chan_image,M,(cols,rows)) 
+        x[:,:,random_channel] = a_chan_image
+        
+    return x
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+def apply_augmentation(x): 
+    augmentation_functions = [salt_and_pepper, blur, rotate_90, rotate_180, rotate_270, flip_up, flip_right, 
+                              remove_some_part, his_equ, brightness, poisson, rescale_and_crop, translate_down, 
+                              translate_left, translate_right, translate_up, random_rotate, shearing]
+    
+
+    ## first augmentation    
+    random_augmentaion_function = random.choice(augmentation_functions) 
+    x = random_augmentaion_function(x)
+    
+    ## second augmentaion with probability 1/2
+    if random.randint(0, 1):
+        augmentation_functions.remove(random_augmentaion_function)
+        random_augmentaion_function = random.choice(augmentation_functions) 
+        x = random_augmentaion_function(x)  
+        
+    return x 
+    
+#----------------------------------------------------------------------------------------------------------------------
+
+    
---
+++ _v5/data_preprocess.py
@@ -0,0 +1,161 @@
+
+import numpy as np
+import os, cv2, sys 
+import numpy as np
+import glob  
+from PIL import Image   
+import pandas as pd 
+
+
+#----------------------------------------------------------------------------------------------------------------------
+
+train_path = 'C:\\Users\\HP\\Downloads\\train\\'
+test_path = 'C:\\Users\\HP\\Downloads\\test\\'
+
+train_np_save_path = "train_np_files\\" 
+train_np_save_label_path = "train_np_files_labels\\" 
+test_np_save_path = "test_np_files\\" 
+
+input_size = (128, 128, 6)
+
+train_data = []
+train_label = []
+test_data = []
+test_label = []
+
+
+experiment_keys   = ['HEPG2', 'HUVEC', 'RPE', 'U2OS']
+experiment_counts = [7750*2,  17688*2, 7753*2, 3324*2]
+
+df = pd.read_csv('train.csv')
+N = df.shape[0] 
+
+print ("The train data processing is started.!")
+for key in range(len(experiment_keys)):
+    
+    index = 0
+    data_container  = np.zeros(shape=(experiment_counts[key], input_size[0], input_size[0], 6), dtype=np.float16)
+    label_container = np.zeros(shape=(experiment_counts[key], 1),                               dtype=np.float16)
+    
+    for i in range(N): 
+        print (df['id_code'][i])
+        
+        experiment = df['experiment'][i]
+        
+        if experiment.split("-")[0] == experiment_keys[key]:
+            plate = df['plate'][i]
+            well = df['well'][i]
+            
+
+            for site in [1, 2]: 
+                image_template = np.zeros(input_size, dtype=np.float16) 
+                
+                for channel in [1,2,3,4,5,6]:
+                    image_path = train_path + str(experiment) + "\\Plate" + str(plate.item()) + "\\" + str(well) + "_s" + str(site) + "_w" + str(channel) + ".png"
+                    image_template[:,:,channel-1] = cv2.resize(cv2.imread(image_path, 0), (128, 128))
+                
+                data_container[index, :,:,:] = image_template.copy()
+                label_container[index] = int(df['sirna'][i])
+                index += 1
+                
+    np.save("train_" + experiment_keys[key] + "_data",  data_container)
+    np.save("train_" + experiment_keys[key] + "_label", label_container)
+    
+
+#----------------------------------------------------------------------------------------------------------------------
+
+experiment_keys   = ['HEPG2', 'HUVEC', 'RPE', 'U2OS']
+experiment_counts = [4429*2,    8846*2,     4417*2,  2205*2]
+
+## process the test data
+df  = pd.read_csv('test.csv') 
+N = df.shape[0]
+
+print ("The test data processing is started.!") 
+for key in range(len(experiment_keys)):
+    
+    index = 0
+    data_container  = np.zeros(shape=(experiment_counts[key], input_size[0], input_size[0], 6), dtype=np.float16)
+    label_container = np.zeros(shape=(experiment_counts[key], 1),                               dtype=np.float16)
+    
+    for i in range(N): 
+        print (df['id_code'][i])
+        
+        experiment = df['experiment'][i]
+        
+        if experiment.split("-")[0] == experiment_keys[key]:
+            plate = df['plate'][i]
+            well = df['well'][i]
+            
+
+            for site in [1, 2]: 
+                image_template = np.zeros(input_size, dtype=np.float16) 
+                
+                for channel in [1,2,3,4,5,6]:
+                    image_path = test_path + str(experiment) + "\\Plate" + str(plate.item()) + "\\" + str(well) + "_s" + str(site) + "_w" + str(channel) + ".png"
+                    image_template[:,:,channel-1] = cv2.resize(cv2.imread(image_path, 0), (128, 128))
+                
+                data_container[index, :,:,:] = image_template.copy() 
+                index += 1
+                
+    np.save("test_" + experiment_keys[key] + "_data", data_container) 
+    
+sys.exit(1)
+
+#----------------------------------------------------------------------------------------------------------------------
+sys.exit(1)
+#----------------------------------------------------------------------------------------------------------------------
+batch_size            = 32
+epochs                = 200
+input_size            = (256, 256, 6)
+path_train_input      = 'I:\\Cellular\\saved_npy_data\\train\\'  
+path_valid_input      = 'I:\\Cellular\\saved_npy_data\\validation\\'    
+load_model_flag       = False
+load_model_path       = 'weights-improvement-200-0.00.hdf5' 
+
+max_sirna_value  = 1107.0
+train_data_count = 63676
+valid_data_count = 9354
+
+train_data  = np.zeros(shape=(train_data_count, 256, 256, 6), dtype=np.float16)
+train_label = np.zeros(shape=(train_data_count, 1), dtype=np.float16)
+# valid_data  = np.zeros(shape=(valid_data_count, 256, 256, 6), dtype=np.float16)
+# valid_label = np.zeros(shape=(valid_data_count, 1), dtype=np.float16)
+
+    
+# idx = 0   
+# for i in range(0, valid_data_count, 1000):  
+    # print (path_valid_input + "valid_data_"  +str(i) + ".npy")
+    # data = np.load(path_valid_input + "valid_data_"  +str(i) + ".npy") 
+    # data = data.astype(np.int16)
+    # resized_data = np.zeros(shape=(data.shape[0], input_size[0], input_size[0], 6), dtype=np.float16)
+    # print (data.shape)
+    
+    # for index in range(resized_data.shape[0]):
+        # for channel in range(6): 
+            # resized_data[index, :,:,channel] = cv2.resize(data[index, :,:,channel], (256, 256)) 
+    # valid_data [idx:idx+resized_data.shape[0], :, :, :] = resized_data.copy()
+    # valid_label[idx:idx+resized_data.shape[0], :]       = np.load(path_valid_input + "valid_laebl_" +str(i) + ".npy")
+    # idx += resized_data.shape[0]
+
+# np.save("I:\\Cellular\\saved_npy_data\\validation\\all_valid_data_256.npy", valid_data)
+# np.save("I:\\Cellular\\saved_npy_data\\validation\\all_valid_label_256.npy", valid_label)
+
+
+idx = 0   
+for i in range(0, train_data_count, 1000):  
+    print (path_train_input + "train_data_"  +str(i) + ".npy")
+    data = np.load(path_train_input + "train_data_"  +str(i) + ".npy") 
+    data = data.astype(np.int16)
+    resized_data = np.zeros(shape=(data.shape[0], input_size[0], input_size[0], 6), dtype=np.float16)
+    print (data.shape)
+    
+    for index in range(resized_data.shape[0]):
+        for channel in range(6): 
+            resized_data[index, :,:,channel] = cv2.resize(data[index, :,:,channel], (256, 256)) 
+    train_data [idx:idx+resized_data.shape[0], :, :, :] = resized_data.copy()
+    train_label[idx:idx+resized_data.shape[0], :]       = np.load(path_train_input + "train_laebl_" +str(i) + ".npy")
+    idx += resized_data.shape[0]
+
+np.save("I:\\Cellular\\saved_npy_data\\validation\\all_train_data_256.npy", train_data)
+np.save("I:\\Cellular\\saved_npy_data\\validation\\all_train_label_256.npy", train_label)
